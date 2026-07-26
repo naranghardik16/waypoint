9c8ba4d7602359832a0d1f1090d599d938e3bad9
Hardik Narang <naranghardik16@gmail.com>
2026-07-27 02:42:04 +0800

    feat: add clerk orgs

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,10 @@ export default function RootLayout({
       )}
     >
       <body>
-        <ClerkProvider appearance={{ theme: shadcn }}>
+        <ClerkProvider
+          appearance={{ theme: shadcn }}
+          taskUrls={{ "choose-organization": "/session-tasks/choose-organization" }}
+        >
           <ThemeProvider>
             {children}
             <Toaster />
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { auth } from "@clerk/nextjs/server"
 
 export default async function Page() {
   await auth.protect()
 
   return (
-    <div className="flex min-h-svh items-center justify-center">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-4">
       <UserButton />
+      <OrganizationSwitcher />
     </div>
   )
 }
